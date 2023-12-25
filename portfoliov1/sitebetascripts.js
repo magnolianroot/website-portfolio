95b76f63f466081b623303bb9af7599934897d09
Victoria Joseph <110946840+magnolianroot@users.noreply.github.com>
2023-12-25 10:25:24 -0500

    Update sitebetascripts.js

--- portfoliov1/sitebetascripts.js
+++ portfoliov1/sitebetascripts.js
@@ -1,25 +1,15 @@
+//back-to-top button 
+let window = document;
+window.onscroll = function () {
+  let topBtn = document.getElementById("topFunction");
 
-
-
-// Get the button:
-const topFunction = document.getElementById("back-to-top-btn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    backToTopButton.style.display = "block";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topBtn.style.display = "block";
   } else {
-    backToTopButton.style.display = "none";
+      topBtn.style.display = "none";
   }
-}
+};
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
 
 
  
@@ -31,6 +21,15 @@ $('#myAlert').on('closed.bs.alert', function () {
 $('myAlert').alert('dispose')
 
 
+
+// $('#myModal').modal(options)
+//alert modal
+
+console.log('Hello from script.js!');
+
+
+
+
 function greet(name) {
   return "Hello, " + name + "!";
 }
@@ -40,4 +39,16 @@ function greet(name) {
 // $('#myModal').modal(options)
 //alert modal
 
-console.log('Hello from script.js!');
+
+
+function nextSlide() {
+  $('myCarouel').carousel('next');
+
+}
+
+//scroll function
+window.addEventListener('scroll', function() {
+  document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
+});
+
+
