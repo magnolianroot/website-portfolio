092461a5ce8bf9ee66d4481d51f5b18a07a035d4
Victoria Joseph | magnolianroot <110946840+magnolianroot@users.noreply.github.com>
2023-12-31 12:22:53 -0500

    Update sitebetascripts.js

--- portfoliov1/sitebetascripts.js
+++ portfoliov1/sitebetascripts.js
@@ -11,8 +11,106 @@ window.onscroll = function () {
 };
 
 
+//scroll function
+window.addEventListener('scroll', function(){
+  this.document.getElementById('showScroll').innerHTML = '';
+  
+})
 
- 
+
+// popper
+const button = document.querySelector('#button');
+const tooltip = document.querySelector('#tooltip');
+
+const popperInstance = Popper.createPopper(button, tooltip, {
+  modifiers: [
+    {
+      name: 'offset',
+      options: {
+        offset: [0, 7],
+        placement: 'top',
+      },
+    },
+  ],
+});
+                        
+
+function show() {
+  tooltip.setAttribute('data-show', '');
+
+  // We need to tell Popper to update the tooltip position
+  // after we show the tooltip, otherwise it will be incorrect
+  popperInstance.update();
+}
+
+function hide() {
+  tooltip.removeAttribute('data-show');
+}
+
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
+
+showEvents.forEach((event) => {
+  button.addEventListener(event, show);
+});
+
+hideEvents.forEach((event) => {
+  button.addEventListener(event, hide);
+});
+
+function show() {
+  // Make the tooltip visible
+  tooltip.setAttribute('data-show', '');
+
+  // Enable the event listeners
+  popperInstance.setOptions((options) => ({
+    ...options,
+    modifiers: [
+      ...options.modifiers,
+      { name: 'eventListeners', enabled: true },
+    ],
+  }));
+
+  // Update its position
+  popperInstance.update();
+}
+
+function hide() {
+  // Hide the tooltip
+  tooltip.removeAttribute('data-show');
+
+  // Disable the event listeners
+  popperInstance.setOptions((options) => ({
+    ...options,
+    modifiers: [
+      ...options.modifiers,
+      { name: 'eventListeners', enabled: false },
+    ],
+  }));
+}
+
+
+function show() {
+  tooltip.setAttribute('data-show', '');
+  popperInstance.update();
+}
+
+function hide() {
+  tooltip.removeAttribute('data-show');
+}
+
+const showEvents = ['mouseenter', 'focus'];
+const hideEvents = ['mouseleave', 'blur'];
+
+showEvents.forEach((event) => {
+  button.addEventListener(event, show);
+});
+
+
+hideEvents.forEach((event) => {
+  button.addEventListener(event, hide);
+});
+
 
 $('.alert').alert('close')
 $('#myAlert').on('closed.bs.alert', function () {
@@ -50,5 +148,4 @@ function nextSlide() {
 window.addEventListener('scroll', function() {
   document.getElementById('showScroll').innerHTML = window.pageYOffset + 'px';
 });
-
 
