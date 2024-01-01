f34688696324b984c9ee04d0e682b6072093bac7
Victoria Joseph | magnolianroot <110946840+magnolianroot@users.noreply.github.com>
2024-01-01 04:54:20 -0500

    Update notes.js

--- portfoliov1/notes.js
+++ portfoliov1/notes.js
@@ -1,19 +1,1 @@
-// code notes: 2 ways to make an array -  'Array' constructor or shorthand
-// literal notation w sqaure brackets '[]'
-let myArray = new Array();
-let myArrayLiteral = [];
-  
-for (let i = 0; i < 10; i++) {
 
-
-}
-
-
-//array beta ver
-
-let toyBox = ['candy', 'cake', 'stuffed bear', 'guitar pick', 'vape'];
-
-console.log(toyBox[1]);
-console.log(toyBox[4]);
-
-toyBox.push('chocolate');
